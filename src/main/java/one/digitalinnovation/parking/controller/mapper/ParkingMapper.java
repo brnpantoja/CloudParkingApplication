481a7cac412c080c23d35cdc0edc1c7a46d0b04e
brnpantoja <treinadorbrunopantoja@gmail.com>
2022-11-15 10:28:50 -0300

    Implementando métodos na classe Parking

--- src/main/java/one/digitalinnovation/parking/controller/mapper/ParkingMapper.java
+++ src/main/java/one/digitalinnovation/parking/controller/mapper/ParkingMapper.java
@@ -6,6 +6,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ParkingMapper {
@@ -13,9 +14,11 @@ public class ParkingMapper {
     private static final ModelMapper MODEL_MAPPER = new ModelMapper();
 
     public ParkingDTO parkingDTO(Parking parking){
+
         return MODEL_MAPPER.map(parking, ParkingDTO.class);
     }
     public List<ParkingDTO> toParkingDTOList(List<Parking> parkingList) {
-        return null;
+
+        return parkingList.stream().map(this::parkingDTO).collect(Collectors.toList());
     }
 }
