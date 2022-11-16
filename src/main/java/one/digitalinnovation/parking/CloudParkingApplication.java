9f96b1915eb11d5227d54fa164b6b1ee3d9db91a
brnpantoja <treinadorbrunopantoja@gmail.com>
2022-11-16 13:51:06 -0300

    Documentando API com Swagger utilizando Spring Fox

--- src/main/java/one/digitalinnovation/parking/CloudParkingApplication.java
+++ src/main/java/one/digitalinnovation/parking/CloudParkingApplication.java
@@ -2,6 +2,8 @@ package one.digitalinnovation.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class CloudParkingApplication {
